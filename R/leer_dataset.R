c59569e91c6c61b6e22891ba88e04fdd87d9ce0c
Juan Manuel Berros <juanmaberros@gmail.com>
2018-07-14 00:37:29 -0300

    Mete nombres_columnas adentro de leer_dataset y agrega obs_id

--- R/leer_dataset.R
+++ R/leer_dataset.R
@@ -1,22 +1,23 @@
 library(tidyverse)
 
-nombres_castellano <- c(
-  "Alcohol" = "alcohol",
-  "MalicAcid" = "acido_malico",
-  "Ash" = "ceniza",
-  "AshAlcalinity" = "alcalinidad_ceniza",
-  "Magnesium" = "magnesio",
-  "TotalPhenols" = "fenoles_totales",
-  "Flavanoids" = "flavonoides",
-  "NonflavanoidPhenols" = "fenoles_no_flavonoides",
-  "Proanthocyanins" = "proantocianinas",
-  "ColorIntensity" = "intensidad_del_color",
-  "Hue" = "tonalidad",
-  "OD280_OD315" = "OD280_OD315",
-  "Proline" = "prolina"
-)
-
 leer_dataset <- function(filename) {
+  nombres_castellano <- c(
+    "Alcohol" = "alcohol",
+    "MalicAcid" = "acido_malico",
+    "Ash" = "ceniza",
+    "AshAlcalinity" = "alcalinidad_ceniza",
+    "Magnesium" = "magnesio",
+    "TotalPhenols" = "fenoles_totales",
+    "Flavanoids" = "flavonoides",
+    "NonflavanoidPhenols" = "no_flavonoides",
+    "Proanthocyanins" = "proantocianinas",
+    "ColorIntensity" = "intensidad_del_color",
+    "Hue" = "tonalidad",
+    "OD280_OD315" = "OD280_OD315",
+    "Proline" = "prolina"
+  )
+
   read_delim(filename, delim = " ") %>%
-  plyr::rename(nombres_castellano)
+    plyr::rename(nombres_castellano) %>%
+    rowid_to_column("obs_id")
 }

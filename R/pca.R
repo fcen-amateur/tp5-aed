120de27cb2a66fc42c449bae461490d11613e3e5
Gonzalo Barrera Borla <gonzalobb@gmail.com>
2018-07-15 01:08:33 -0300

    Modificaciones ligeras para emparejar con prcomp

--- R/pca.R
+++ R/pca.R
@@ -1,22 +1,31 @@
 library(tidyverse)
+library(stringr)
 
-analisis_de_componentes_principales <- function(df) {
-  X <- data.matrix(df)
-  descomposicion <- svd(cov(X)) # TODO: implementar un `cov` casero?
+analisis_de_componentes_principales <- function(X) {
+  if (is.data.frame(X)) { X <- data.matrix(X) }
+  X <- scale(X)
+
+  descomposicion <- svd(cov(X))
   autovalores <- descomposicion$d
   matriz_de_autovectores <- descomposicion$u
   assertthat::are_equal(descomposicion$u, descomposicion$v)
-  nombres_componentes <- 1:ncol(X) %>% map_chr(~paste("PC", .x, sep = ""))
+  nombres_componentes <- stringr::str_c("PC", 1:ncol(X))
   varianza_total <- sum(autovalores)
   varianza_explicada <- autovalores / varianza_total
   names(varianza_explicada) <- nombres_componentes
   componentes_principales <- (X %*% matriz_de_autovectores) %>% as_tibble
-  colnames(componentes_principales) = nombres_componentes
+  colnames(componentes_principales) <- nombres_componentes
   
   return (list(
-    X = componentes_principales,
-    varianza_total = varianza_total,
+    centro_X = attr(X, "scaled:center"), # prcomp()$center
+    desvio_X = attr(X, "scaled:scale"), # prcomp()$scale
+    Z = componentes_principales, # prcomp()$x
+    autovalores = autovalores,
+    desvio_Z  = sqrt(autovalores), # prcomp()$sdev
+    varianza_total = varianza_total, 
     varianza_explicada = varianza_explicada,
-    matriz_de_autovectores = matriz_de_autovectores
+    matriz_de_autovectores = matriz_de_autovectores # prcomp()$rotation
   ))
 }
+
+acp <- analisis_de_componentes_principales

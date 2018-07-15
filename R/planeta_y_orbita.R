403a0b390c77a8549a47937abe378c388d568964
Gonzalo Barrera Borla <gonzalobb@gmail.com>
2018-07-15 01:09:07 -0300

    Agrego algunos puntos extra al planeta y su anillo

--- R/planeta_y_orbita.R
+++ R/planeta_y_orbita.R
@@ -1,7 +1,7 @@
 set.seed(42)
 
-radios_planeta <- runif( n = 100, min = 0, max= 1 )
-angulos_planeta <- runif( n = 100, min = 0, 2*pi )
+radios_planeta <- runif( n = 200, min = 0, max= 4 )
+angulos_planeta <- runif( n = 200, min = 0, 2*pi )
 
 interpretar_coordenada_x <- function (radio, argumento) {
   return( radio * cos(argumento) )
@@ -16,8 +16,8 @@ x_planeta <- map2_dbl(radios_planeta, angulos_planeta, interpretar_coordenada_x)
 y_planeta <- map2_dbl(radios_planeta, angulos_planeta, interpretar_coordenada_y)
 
 
-radios_anillo <- runif( n = 100, min = 1.5, max= 2.5 )
-angulos_anillo <- runif( n = 100, min = 0, 2*pi )
+radios_anillo <- runif( n = 200, min = 5, max= 5.5 )
+angulos_anillo <- runif( n = 200, min = 0, 2*pi )
 
 x_anillo <- map2_dbl(radios_anillo, angulos_anillo, interpretar_coordenada_x)
 y_anillo <- map2_dbl(radios_anillo, angulos_anillo, interpretar_coordenada_y)
@@ -27,7 +27,7 @@ y <- append(y_planeta,y_anillo)
 
 #Para no hacersela más fácil al algoritmo, vamos a unir las listas usando sample:
 
-rifa <- sample( 1:200, 200, replace=F ) 
+rifa <- sample( 1:length(x), replace=F ) 
 
 planeta_y_anillo <- tibble("x" = x[c(rifa)],"y"= y[c(rifa)] )
 

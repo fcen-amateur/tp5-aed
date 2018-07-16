8ba5d376b6b3d99b29e16024151519fe8525c74a
Octavio <octavio.el.magnifico@gmail.com>
2018-07-16 17:47:57 -0300

    Ahora el gif es precioso.

--- R/armar_gif.R
+++ R/armar_gif.R
@@ -1,5 +1,7 @@
 library("tidyverse")
 
+colores <- brewer.pal(3,"Set2")
+
 X <- readRDS("data/GIF/planeta0.rds")
 contador <- 0
 i <- 0
@@ -12,8 +14,22 @@ while (contador <= 999) {
     X <- X_entrante
     X %>% ggplot( ) +
       aes(x=x,y=y) +
-      geom_point( aes(color=as.factor(clase)) ) +
-      scale_color_brewer(palette = "Set2", name="Clase") +
+      geom_point( aes(color=as.factor(clase),shape=as.factor(clase)) ) +
+      scale_color_manual(name = "Clase Asignada",
+                         values=c("0" = colores[1],
+                                  "1" = colores[2],
+                                  "2" = colores[3]
+#                                  ),
+#                         labels=c("0","1","2")) +
+                                  )) +
+      scale_shape_manual(name="Clasificado",
+                         values=c( "0" = 1,
+                                   "1" = 16,
+                                  "2" = 17),
+                         labels=c("0" = "No Clasificado",
+                                  "1" = "Planeta",
+                                  "2" = "Anillo")
+                         ) +
       ggtitle("Evolución de la Acumulación") +
       
     ggsave( paste0("imgs/GIF/planeta",str_pad(i,4,"left","0"),".png") )
